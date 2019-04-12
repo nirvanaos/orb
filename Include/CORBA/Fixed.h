97a83754fd77e35e7bc180874919d85ffbe87104
Igor Popov <silver.popov@gmail.com>
2019-04-12 05:33:46 +0300

    Fixed.h

--- Include/CORBA/Fixed.h
+++ Include/CORBA/Fixed.h
@@ -2,6 +2,7 @@
 #define NIRVANA_ORB_FIXED_H_
 
 #include "BasicTypes.h"
+#include <iosfwd>
 
 namespace CORBA {
 
@@ -9,9 +10,7 @@ class Fixed
 {
 public:
 	// Constructors
-	Fixed (int val = 0);
-	Fixed (unsigned val);
-	Fixed (Long val);
+	Fixed (Long val = 0);
 	Fixed (ULong val);
 	Fixed (LongLong val);
 	Fixed (ULongLong val);
@@ -22,33 +21,33 @@ public:
 	~Fixed ();
 
 	// Conversions
-	operator LongLong() const;
-	operator LongDouble() const;
+	operator LongLong () const;
+	operator LongDouble () const;
 	Fixed round (UShort scale) const;
 	Fixed truncate (UShort scale) const;
-	char *to_string () const;
+	Char *to_string () const;
 
 	// Operators
-	Fixed& operator=(const Fixed& val);
-	Fixed& operator+=(const Fixed& val);
-	Fixed& operator-=(const Fixed& val);
-	Fixed& operator*=(const Fixed& val);
-	Fixed& operator/=(const Fixed& val);
-	Fixed& operator++();
-	Fixed operator++(int);
-	Fixed& operator--();
-	Fixed operator--(int);
-	Fixed operator+() const;
-	Fixed operator-() const;
-	Boolean operator!() const;
+	Fixed& operator = (const Fixed& val);
+	Fixed& operator += (const Fixed& val);
+	Fixed& operator -= (const Fixed& val);
+	Fixed& operator *= (const Fixed& val);
+	Fixed& operator /= (const Fixed& val);
+	Fixed& operator ++ ();
+	Fixed operator ++ (int);
+	Fixed& operator -- ();
+	Fixed operator -- (int);
+	Fixed operator + () const;
+	Fixed operator - () const;
+	Boolean operator! () const;
 
 	// Other member functions
 	UShort fixed_digits () const;
 	UShort fixed_scale () const;
 };
 
-istream& operator>>(istream& is, Fixed& val);
-ostream& operator<<(ostream& os, const Fixed& val);
+std::istream& operator >> (std::istream& is, Fixed& val);
+std::ostream& operator << (std::ostream& os, const Fixed& val);
 Fixed operator + (const Fixed& val1, const Fixed& val2);
 Fixed operator - (const Fixed& val1, const Fixed& val2);
 Fixed operator * (const Fixed& val1, const Fixed& val2);
