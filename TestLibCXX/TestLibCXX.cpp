bbe8775febab35506772746fce07693fbf2efecf
Igor Popov <popov@intromarin.ru>
2019-09-30 18:11:15 +0300

    STL files added.

---
+++ TestLibCXX/TestLibCXX.cpp
@@ -0,0 +1,42 @@
+//#include <gtest/gtest.h>
+#include <string>
+/*
+namespace std {
+typedef decltype(nullptr) nullptr_t;
+}*/
+
+/*
+namespace TestLibCXX {
+
+using namespace std;
+
+class TestLibCXX :
+	public ::testing::Test
+{
+protected:
+	TestLibCXX ()
+	{}
+
+	virtual ~TestLibCXX ()
+	{}
+
+	// If the constructor and destructor are not enough for setting up
+	// and cleaning up each test, you can define the following methods:
+
+	virtual void SetUp ()
+	{
+		// Code here will be called immediately after the constructor (right
+		// before each test).
+	}
+
+	virtual void TearDown ()
+	{
+		// Code here will be called immediately after each test (right
+		// before the destructor).
+	}
+};
+
+TEST_F (TestLibCXX, String)
+{}
+
+*/
