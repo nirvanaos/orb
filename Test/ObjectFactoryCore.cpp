ab72d4a6fd455122d2ce14e5f7139a33166aebe5
Igor Popov <popov@intromarin.ru>
2020-01-28 14:59:13 +0300

    Import/export test.

--- Test/ObjectFactoryCore.cpp
+++ Test/ObjectFactoryCore.cpp
@@ -11,3 +11,14 @@ extern "C" __declspec (allocate(OLF_BIND))
 const Nirvana::ExportInterface _exp_CORBA_Nirvana_object_factory {
 	Nirvana::OLF_EXPORT_INTERFACE, "CORBA/Nirvana/g_object_factory", STATIC_BRIDGE (CORBA::Nirvana::ObjectFactoryCore, CORBA::Nirvana::ObjectFactory)
 };
+
+namespace CORBA {
+namespace Nirvana {
+
+__declspec (allocate(OLF_BIND))
+const ::Nirvana::ImportInterfaceT <ObjectFactory> g_object_factory = {
+	::Nirvana::OLF_IMPORT_INTERFACE, "CORBA/Nirvana/g_object_factory", ObjectFactory::interface_id_
+};
+
+}
+}
