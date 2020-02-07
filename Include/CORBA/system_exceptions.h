c4b8c00fed29954476cbeb80e3f20bf431a63361
Igor Popov <popov@intromarin.ru>
2020-02-07 07:56:05 +0300

    Binary search exception.

--- Include/CORBA/system_exceptions.h
+++ Include/CORBA/system_exceptions.h
@@ -1,49 +1,52 @@
 /// Macros to define all system exceptions.
+/// Order is significant because exceptions must be sorted by repo id.
 #ifndef NIRVANA_ORB_SYSTEM_EXCEPTIONS_H_
 #define NIRVANA_ORB_SYSTEM_EXCEPTIONS_H_
 
+/// CORBA system exceptions
 #define CORBA_EXCEPTIONS(M)/*
-*/ M (UNKNOWN) /* the unknown exception
-*/ M (BAD_PARAM) /* an invalid parameter was passed
-*/ M (NO_MEMORY) /* dynamic memory allocation failure
-*/ M (IMP_LIMIT) /* violated implementation limit
-*/ M (COMM_FAILURE) /* communication failure
-*/ M (INV_OBJREF) /* invalid object reference
-*/ M (NO_PERMISSION) /* no permission for attempted op.
-*/ M (INTERNAL) /* ORB internal error
-*/ M (MARSHAL) /* error marshalling param/result
-*/ M (INITIALIZE) /* ORB initialization failure
-*/ M (NO_IMPLEMENT) /* operation implementation unavailable
-*/ M (BAD_TYPECODE) /* bad typecode
+*/ M (BAD_CONTEXT) /* error processing context object
+*/ M (BAD_INV_ORDER) /* routine invocations out of order
 */ M (BAD_OPERATION) /* invalid operation
+*/ M (BAD_PARAM) /* an invalid parameter was passed
+*/ M (BAD_QOS) /* bad quality of service
+*/ M (BAD_TYPECODE) /* bad typecode
+*/ M (CODESET_INCOMPATIBLE) /* incompatible code set
+*/ M (COMM_FAILURE) /* communication failure
+*/ M (DATA_CONVERSION) /* data conversion error
+*/ M (FREE_MEM) /* cannot free memory
+*/ M (IMP_LIMIT) /* violated implementation limit
+*/ M (INITIALIZE) /* ORB initialization failure
+*/ M (INTERNAL) /* ORB internal error
+*/ M (INTF_REPOS) /* error accessing interface repository
+*/ M (INVALID_TRANSACTION) /* invalid transaction
+*/ M (INV_FLAG) /* invalid flag was specified
+*/ M (INV_IDENT) /* invalid identifier syntax
+*/ M (INV_OBJREF) /* invalid object reference
+*/ M (INV_POLICY) /* invalid policy
+*/ M (MARSHAL) /* error marshalling param/result
+*/ M (NO_IMPLEMENT) /* operation implementation unavailable
+*/ M (NO_MEMORY) /* dynamic memory allocation failure
+*/ M (NO_PERMISSION) /* no permission for attempted op.
 */ M (NO_RESOURCES) /* insufficient resources for req.
 */ M (NO_RESPONSE) /* response to req. not yet available
-*/ M (PERSIST_STORE) /* persistent storage failure
-*/ M (BAD_INV_ORDER) /* routine invocations out of order
-*/ M (TRANSIENT) /* transient failure - reissue request
-*/ M (FREE_MEM) /* cannot free memory
-*/ M (INV_IDENT) /* invalid identifier syntax
-*/ M (INV_FLAG) /* invalid flag was specified
-*/ M (INTF_REPOS) /* error accessing interface repository
-*/ M (BAD_CONTEXT) /* error processing context object
-*/ M (OBJ_ADAPTER) /* failure detected by object adapter
-*/ M (DATA_CONVERSION) /* data conversion error
 */ M (OBJECT_NOT_EXIST) /* non-existent object, delete reference
-*/ M (TRANSACTION_REQUIRED) /* transaction required
-*/ M (TRANSACTION_ROLLEDBACK) /* transaction rolled back
-*/ M (INVALID_TRANSACTION) /* invalid transaction
-*/ M (INV_POLICY) /* invalid policy
-*/ M (CODESET_INCOMPATIBLE) /* incompatible code set
+*/ M (OBJ_ADAPTER) /* failure detected by object adapter
+*/ M (PERSIST_STORE) /* persistent storage failure
 */ M (REBIND) /* rebind needed
 */ M (TIMEOUT) /* operation timed out
-*/ M (TRANSACTION_UNAVAILABLE) /* no transaction
 */ M (TRANSACTION_MODE) /* invalid transaction mode
-*/ M (BAD_QOS) /* bad quality of service
+*/ M (TRANSACTION_REQUIRED) /* transaction required
+*/ M (TRANSACTION_ROLLEDBACK) /* transaction rolled back
+*/ M (TRANSACTION_UNAVAILABLE) /* no transaction
+*/ M (TRANSIENT) /* transient failure - reissue request
+*/ M (UNKNOWN) /* the unknown exception
 */
 
+/// Nirvana system exceptions
 #define NIRVANA_EXCEPTIONS(M)/*
-*/ M (MEM_NOT_COMMITTED) /* memory is not committed
 */ M (MEM_NOT_ALLOCATED) /* memory is not allocated
+*/ M (MEM_NOT_COMMITTED) /* memory is not committed
 */
 
 #define SYSTEM_EXCEPTIONS(M) \
--- Source/SystemException.cpp
+++ Source/SystemException.cpp
@@ -2,6 +2,7 @@
 #include <CORBA/RepositoryId.h>
 #include <CORBA/TypeCode.h>
 #include <algorithm>
+#include <string.h>
 
 #define EX_TABLE_ENTRY(e) { STATIC_BRIDGE (Nirvana::TypeCodeException <e>, TypeCode) },
 
@@ -10,18 +11,32 @@ namespace CORBA {
 using namespace Nirvana;
 using namespace std;
 
-const ExceptionEntry SystemException::type_codes_ [SystemException::KNOWN_SYSTEM_EXCEPTIONS] = {
+ExceptionEntry SystemException::type_codes_ [SystemException::KNOWN_SYSTEM_EXCEPTIONS] = {
 	SYSTEM_EXCEPTIONS (EX_TABLE_ENTRY)
 };
 
 TypeCode_ptr SystemException::_get_type_code (const char* rep_id, int hint)
 {
+	struct TC_Pred
+	{
+		bool operator () (ExceptionEntry& l, const char* r) const
+		{
+			return strcmp (l->id (), r) < 0;
+		}
+
+		bool operator () (const char* l, ExceptionEntry& r) const
+		{
+			return strcmp (l, r->id ()) < 0;
+		}
+	};
+
 	if (hint >= 0 && hint < KNOWN_SYSTEM_EXCEPTIONS) {
 		TypeCode_ptr tc = type_codes_ [hint];
 		if (Nirvana::RepositoryId::compatible (tc->id (), rep_id))
 			return tc;
 	} else {
-		for (ExceptionEntry* pe = begin (type_codes_); pe != end (type_codes_); ++pe) {
+		ExceptionEntry* pe = lower_bound (begin (type_codes_), end (type_codes_), rep_id, TC_Pred ());
+		if (pe != end (type_codes_)) {
 			TypeCode_ptr tc = *pe;
 			if (Nirvana::RepositoryId::compatible (tc->id (), rep_id))
 				return tc;
