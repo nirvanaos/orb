b1314ee6707792e25fff01acc1f68c05dd5ba535
Igor Popov <popov.nirvana@gmail.com>
2024-02-27 20:11:21 +0300

    Remove redundant sources

--- Mock/tc_TypeCode.cpp
+++
@@ -1,9 +0,0 @@
-#include <CORBA/CORBA.h>
-
-namespace CORBA {
-
-const Nirvana::ImportInterfaceT <TypeCode> _tc_TCKind = { Nirvana::OLF_IMPORT_INTERFACE };
-const Nirvana::ImportInterfaceT <TypeCode> Internal::Decls <TypeCode>::_tc_BadKind = { ::Nirvana::OLF_IMPORT_INTERFACE };
-const Nirvana::ImportInterfaceT <TypeCode> Internal::Decls <TypeCode>::_tc_Bounds = { ::Nirvana::OLF_IMPORT_INTERFACE };
-
-}
